e367eae5a7f5c9c209258ba631ed7e4c9f9c0dce
Ryohei Fujihara <giver.yell@gmail.com>
2021-10-17 17:25:32 +0900

    useState textInput

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Article from './components/Article.jsx';
+import TextInput from './components/TextInput.jsx';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         title={'タイトル'}
         content={'コンテント'}
       />
+      <TextInput />
     </div>
   );
 }
